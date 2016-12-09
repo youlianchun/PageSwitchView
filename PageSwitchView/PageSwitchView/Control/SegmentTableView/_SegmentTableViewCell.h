d546b2c78ef4c4db1a1f4d7cf164b23c8d007aa1
YLCHUN <985050622@qq.com>
2016-12-09 16:54:49 +0800

    目录结构整理

---
+++ PageSwitchView/PageSwitchView/Control/SegmentTableView/_SegmentTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  _SegmentTableViewCell.h
+//  PageSwitchView
+//
+//  Created by YLCHUN on 16/12/9.
+//  Copyright © 2016年 ylchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _SegmentTableViewCell : UITableViewCell
+@property (nonatomic) NSInteger number;
+@end
