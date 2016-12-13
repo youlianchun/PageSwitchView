530248fdd8864e390b6ec700868764a2ff0b542a
YLCHUN <985050622@qq.com>
2016-12-13 13:12:38 +0800

    no message

--- PageSwitchView/PageSwitchView/Control/UIContentView/UIContentView.m
+++ PageSwitchView/PageSwitchView/Control/UIContentView/UIContentView.m
@@ -7,42 +7,12 @@
 //
 
 #import "UIContentView.h"
-#import <objc/runtime.h>
 
 #pragma mark -
 #pragma mark - _UIContentView
 @interface _UIContentView : UIView
 @end
 @implementation _UIContentView
-@end
-
-#pragma mark -
-#pragma mark - UIView(UIContentView)
-@implementation UIView(UIContentView)
-+(void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        SEL originalSelector = @selector(superview);
-        SEL swizzledSelector = @selector(superview_contentView);
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        BOOL success = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
-        if (success) {
-            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-    });
-}
--(UIView *)superview_contentView {
-    UIView *superview = [self superview_contentView];
-    if ([superview isKindOfClass:[_UIContentView class]]) {
-        return superview.superview;
-    }else {
-        return superview;
-    }
-}
 @end
 
 #pragma mark -
@@ -68,7 +38,7 @@
 
 -(UIView *)contentView {
     if (!_contentView) {
-        _contentView = [[_UIContentView alloc]init];
+        _contentView = [[_UIContentView alloc] init];
         _contentView.backgroundColor = [UIColor clearColor];
         _contentView.opaque = NO;
         [super addSubview:_contentView];
